74062e210b29b556fb50f0c2399b45c9744f5731
Anna Carolina Zomer <117862465+Anz0mer@users.noreply.github.com>
2023-11-16 09:46:46 -0300

    Update Gerente.java

--- POO/src/model/Gerente.java
+++ POO/src/model/Gerente.java
@@ -5,4 +5,8 @@ public class Gerente extends Pessoa {
     public Gerente(String nome, String cpf) {
         super(nome, cpf);
     }
+
+    public Gerente(String nome, String cpf, String senha, String valor, String conta) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
